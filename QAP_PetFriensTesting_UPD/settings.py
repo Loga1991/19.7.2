8eaf85da3300d462fb9d8f7f09a3128b1f0d784c
Loga1991 <125743391+Loga1991@users.noreply.github.com>
2023-06-07 16:41:16 +0300

    Add files via upload

---
+++ QAP_PetFriensTesting_UPD/settings.py
@@ -0,0 +1,10 @@
+
+valid_email = "vasya@mail.com"
+valid_password = "12345"
+
+unvalid_email = "vasya1@mail.com"
+unvalid_password = "123456"
+
+valid_email_1='lozhkinagv@gmail.com'
+valid_password_2='1234'
+
